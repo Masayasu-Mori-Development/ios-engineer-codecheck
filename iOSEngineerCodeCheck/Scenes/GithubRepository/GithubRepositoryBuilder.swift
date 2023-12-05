a683d874f124e3301c0943f5ac1d1ff47eec378e
MasayasuMori <masayasu.mori.develop@gmail.com>
2023-12-05 09:08:14 +0900

    Add GithubRepositoryBuilder logic

--- iOSEngineerCodeCheck/Scenes/GithubRepository/GithubRepositoryBuilder.swift
+++ iOSEngineerCodeCheck/Scenes/GithubRepository/GithubRepositoryBuilder.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+protocol GithubRepositoryBuilderProtocol: SceneBuilder
+where ViewController == GithubRepositoryViewController {
+    func build(repository: [String: Any]) -> ViewController
+}
+
+final class GithubRepositoryBuilder: GithubRepositoryBuilderProtocol {
+    func build(repository: [String : Any]) -> ViewController {
+        let viewController = defaultBuild()
+        let presenter = GithubRepositoryPresenter(
+            repository: repository
+        )
+        viewController.inject(presenter: presenter)
+
+        return viewController
+    }
+}
+
