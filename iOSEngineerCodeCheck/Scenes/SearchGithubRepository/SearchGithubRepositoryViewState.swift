c1bb462589fc204555782e84b712a9cc73e4c26c
MasayasuMori <masayasu.mori.develop@gmail.com>
2023-12-05 10:20:29 +0900

    Add SearchGithubRepositoryViewState file

---
+++ iOSEngineerCodeCheck/Scenes/SearchGithubRepository/SearchGithubRepositoryViewState.swift
@@ -0,0 +1,9 @@
+//
+//  SearchGithubRepositoryViewState.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 森勝康 on 2023/12/05.
+//  Copyright © 2023 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
