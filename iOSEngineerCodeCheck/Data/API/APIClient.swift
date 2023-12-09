cb6c62d43023046ef8dc727e6f524fce60176ade
MasayasuMori <masayasu.mori.develop@gmail.com>
2023-12-09 12:35:27 +0900

    Add APIClient

---
+++ iOSEngineerCodeCheck/Data/API/APIClient.swift
@@ -0,0 +1,13 @@
+//
+//  APIClient.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 森勝康 on 2023/12/09.
+//  Copyright © 2023 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+final class APIClient {
+    
+}
