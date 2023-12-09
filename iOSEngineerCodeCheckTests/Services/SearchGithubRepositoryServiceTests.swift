ef42a65b0ff4025f4ee753acbea419b5013da61e
MasayasuMori <masayasu.mori.develop@gmail.com>
2023-12-09 11:50:43 +0900

    Add search word is empty error

--- iOSEngineerCodeCheckTests/Services/SearchGithubRepositoryServiceTests.swift
+++ iOSEngineerCodeCheckTests/Services/SearchGithubRepositoryServiceTests.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2023 YUMEMI Inc. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import iOSEngineerCodeCheck
+
+class SearchGithubRepositoryServiceTests: XCTestCase {
+    let service = SearchGithubRepositoryService()
+    
+    func test空文字で検索してエラーが返ってくるか() async {
+        do {
+            let _ = try await service.searchGithubRepositories(word: "")
+        } catch {
+            guard let searchError = error as? SearchGithubRepositoryService.SearchGithubRepositoryError else {
+                XCTExpectFailure("")
+                return
+            }
+            XCTAssertEqual(searchError, .wordIsEmpty)
+        }
+    }
+}
