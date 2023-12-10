6c92e7636e687b1784c5f04e4dc640fce347b5ff
MasayasuMori <masayasu.mori.develop@gmail.com>
2023-12-10 12:41:06 +0900

    Add GithubRepository

--- iOSEngineerCodeCheck/Data/API/APIClient.swift
+++ iOSEngineerCodeCheck/Data/API/APIClient.swift
@@ -7,7 +7,51 @@
 //
 
 import Foundation
+import Combine
 
-final class APIClient {
-    
+protocol APIClientProtocol {
+    func request<T>(_ apiRequest: T) async throws -> T.ResponseType where T: APIRequestProtocol
+    func cancel()
+}
+
+final class APIClient: APIClientProtocol {
+    private var sessionTask: URLSessionTask?
+    private var jsonDecoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
+
+    func request<T>(_ apiRequest: T) async throws -> T.ResponseType where T: APIRequestProtocol {
+        let url = try createURL(request: apiRequest)
+
+        return try await withCheckedThrowingContinuation { continuation in
+            self.sessionTask = URLSession.shared.dataTask(with: url) { (data, _, _) in
+                if let data,
+                   let responseData = try? self.jsonDecoder.decode(T.ResponseType.self, from: data) {
+                    continuation.resume(returning: responseData)
+                } else {
+                    continuation.resume(throwing: APIError.requestFailed)
+                }
+            }
+            self.sessionTask?.resume()
+        }
+    }
+
+    func cancel() {
+        sessionTask?.cancel()
+    }
+
+    private func createURL(request: any APIRequestProtocol) throws -> URL {
+        guard var urlComponent = URLComponents(string: request.baseUrl + request.path) else {
+            throw APIError.cannotCreateURL
+        }
+        urlComponent.queryItems = request.queryParameters.map {
+            URLQueryItem(name: $0.key, value: $0.value)
+        }
+        guard let url = urlComponent.url else {
+            throw APIError.cannotCreateURL
+        }
+        return url
+    }
 }
